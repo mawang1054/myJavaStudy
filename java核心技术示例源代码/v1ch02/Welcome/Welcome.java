8afa4c1afcf623286ef8b9cb66ccc7debcc43c67
mawang1054 <mawang1054@126.com>
2018-12-25 14:14:29 +0800

    12.05

--- java核心技术示例源代码/v1ch02/Welcome/Welcome.java
+++ java核心技术示例源代码/v1ch02/Welcome/Welcome.java
@@ -7,7 +7,7 @@ public class Welcome
 {
    public static void main(String[] args)
    {
-      String greeting = "Welcome to Core Java!";
+      String greeting = "\u0022"+"+"+"\u0022";
       System.out.println(greeting);
       for (int i = 0; i < greeting.length(); i++)
          System.out.print("=");
